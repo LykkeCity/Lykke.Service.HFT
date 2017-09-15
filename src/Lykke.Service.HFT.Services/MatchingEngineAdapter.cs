085a5e614b0dabc64ca59257183b123590aaf0f7
Sergey Polonsky <polonsky.sergey@hotmail.com>
2017-09-16 02:22:09 +0300

    Added API for placing market orders.

--- src/Lykke.Service.HFT.Services/MatchingEngineAdapter.cs
+++ src/Lykke.Service.HFT.Services/MatchingEngineAdapter.cs
@@ -50,12 +50,17 @@ namespace Lykke.Service.HFT.Services
 			return ConvertToApiModel(response);
 		}
 
-		public async Task HandleMarketOrderAsync(string clientId, string assetPairId, Core.Domain.OrderAction orderAction, double volume,
+		public async Task<ResponseModel<Guid>> HandleMarketOrderAsync(string clientId, string assetPairId, Core.Domain.OrderAction orderAction, double volume,
 			bool straight, double? reservedLimitVolume = null)
 		{
-			var id = GetNextRequestId();
-			var matchingEngineId = await _matchingEngineClient.HandleMarketOrderAsync(id, clientId, assetPairId, (OrderAction)orderAction, volume, straight, reservedLimitVolume);
-		}
+			var requestId = GetNextRequestGuid();
+			var matchingEngineResponse = await _matchingEngineClient.HandleMarketOrderAsync(requestId.ToString(), clientId, assetPairId, (OrderAction)orderAction, volume, straight, reservedLimitVolume);
+		    if (!string.IsNullOrEmpty(matchingEngineResponse))
+		    {
+		        return ResponseModel<Guid>.CreateFail(ResponseModel.ErrorCodeType.RuntimeError, matchingEngineResponse);
+		    }
+		    return ResponseModel<Guid>.CreateOk(requestId);
+        }
 
 		public async Task<ResponseModel<Guid>> PlaceLimitOrderAsync(string clientId, string assetPairId, Core.Domain.OrderAction orderAction, double volume,
 			double price, bool cancelPreviousOrders = false)
