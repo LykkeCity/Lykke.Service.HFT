c65e1f7ae2645ac99e06ea67c4b41feafcb2bdce
Sergey Polonsky <polonsky.sergey@hotmail.com>
2017-10-13 18:11:52 +0300

    Refactoring: removed unusing code.

--- src/Lykke.Service.HFT.AzureRepositories/Accounts/AccountsRepository.cs
+++ src/Lykke.Service.HFT.AzureRepositories/Accounts/AccountsRepository.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AzureStorage;
@@ -11,174 +10,101 @@ using Newtonsoft.Json;
 namespace Lykke.Service.HFT.AzureRepositories.Accounts
 {
 
-	public class WalletEntity : TableEntity
-	{
-		public class TheWallet : IWallet
-		{
-			[JsonProperty("balance")]
-			public double Balance { get; set; }
+    public class WalletEntity : TableEntity
+    {
+        public class TheWallet : IWallet
+        {
+            [JsonProperty("balance")]
+            public double Balance { get; set; }
 
-			[JsonProperty("asset")]
-			public string AssetId { get; set; }
+            [JsonProperty("asset")]
+            public string AssetId { get; set; }
 
-			[JsonProperty("reserved")]
-			public double Reserved { get; set; }
-
-
-			public static TheWallet Create(string assetId, double balance)
-			{
-				return new TheWallet
-				{
-					AssetId = assetId,
-					Balance = balance
-				};
-			}
-		}
-
-		public static string GeneratePartitionKey()
-		{
-			return "ClientBalance";
-		}
-
-		public static string GenerateRowKey(string traderId)
-		{
-			return traderId;
-		}
-
-		public string ClientId => RowKey;
-
-		public string Balances { get; set; }
-
-		internal void UpdateBalance(string assetId, double balanceDelta)
-		{
-			var data = Get();
-			var element = data.FirstOrDefault(itm => itm.AssetId == assetId);
-
-			if (element != null)
-			{
-				element.Balance += balanceDelta;
-				Balances = data.ToJson();
-				return;
-			}
-
-			var list = new List<TheWallet>();
-			list.AddRange(data);
-			list.Add(TheWallet.Create(assetId, balanceDelta));
-			Balances = list.ToJson();
-
-		}
-
-		internal static readonly TheWallet[] EmptyList = new TheWallet[0];
-
-		internal TheWallet[] Get()
-		{
-			if (string.IsNullOrEmpty(Balances))
-				return EmptyList;
-
-			return Balances.DeserializeJson(() => EmptyList);
-		}
-		public static WalletEntity Create(string clientId)
-		{
-			return new WalletEntity
-			{
-				PartitionKey = GeneratePartitionKey(),
-				RowKey = GenerateRowKey(clientId),
-
-			};
-		}
-	}
-
-	public class WalletsRepository : IWalletsRepository
-	{
-		private readonly INoSQLTableStorage<WalletEntity> _tableStorage;
+            [JsonProperty("reserved")]
+            public double Reserved { get; set; }
 
 
+            public static TheWallet Create(string assetId, double balance)
+            {
+                return new TheWallet
+                {
+                    AssetId = assetId,
+                    Balance = balance
+                };
+            }
+        }
 
-		public WalletsRepository(INoSQLTableStorage<WalletEntity> tableStorage)
-		{
-			_tableStorage = tableStorage;
-		}
+        public static string GeneratePartitionKey()
+        {
+            return "ClientBalance";
+        }
 
-		public async Task<IEnumerable<IWallet>> GetAsync(string traderId)
-		{
-			var partitionKey = WalletEntity.GeneratePartitionKey();
-			var rowKey = WalletEntity.GenerateRowKey(traderId);
-			var entity = await _tableStorage.GetDataAsync(partitionKey, rowKey);
+        public static string GenerateRowKey(string traderId)
+        {
+            return traderId;
+        }
 
-			return entity == null
-				? WalletEntity.EmptyList
-				: entity.Get();
-		}
+        public string ClientId => RowKey;
 
-		public async Task<IWallet> GetAsync(string traderId, string assetId)
-		{
-			var entities = await GetAsync(traderId);
-			return entities.FirstOrDefault(itm => itm.AssetId == assetId);
-		}
+        public string Balances { get; set; }
 
-		public Task UpdateBalanceAsync(string traderId, string assetId, double balance)
-		{
-			var partitionKey = WalletEntity.GeneratePartitionKey();
-			var rowKey = WalletEntity.GenerateRowKey(assetId);
+        internal void UpdateBalance(string assetId, double balanceDelta)
+        {
+            var data = Get();
+            var element = data.FirstOrDefault(itm => itm.AssetId == assetId);
 
-			return _tableStorage.InsertOrModifyAsync(partitionKey, rowKey,
+            if (element != null)
+            {
+                element.Balance += balanceDelta;
+                Balances = data.ToJson();
+                return;
+            }
 
-				() =>
-				{
-					var newEntity = WalletEntity.Create(traderId);
-					newEntity.UpdateBalance(assetId, balance);
-					return newEntity;
-				},
+            var list = new List<TheWallet>();
+            list.AddRange(data);
+            list.Add(TheWallet.Create(assetId, balanceDelta));
+            Balances = list.ToJson();
 
-				entity =>
-				{
-					entity.UpdateBalance(assetId, balance);
-					return entity;
-				}
+        }
 
-			);
-		}
+        internal static readonly TheWallet[] EmptyList = new TheWallet[0];
 
-		public async Task<Dictionary<string, double>> GetTotalBalancesAsync()
-		{
-			var result = new Dictionary<string, double>();
+        internal TheWallet[] Get()
+        {
+            if (string.IsNullOrEmpty(Balances))
+                return EmptyList;
 
-			await _tableStorage.GetDataByChunksAsync(entities =>
-			{
-				foreach (var walletEntity in entities)
-				foreach (var balances in walletEntity.Get())
-				{
-					if (!result.ContainsKey(balances.AssetId))
-						result.Add(balances.AssetId, balances.Balance);
-					else
-						result[balances.AssetId] += balances.Balance;
-				}
-			});
+            return Balances.DeserializeJson(() => EmptyList);
+        }
+        public static WalletEntity Create(string clientId)
+        {
+            return new WalletEntity
+            {
+                PartitionKey = GeneratePartitionKey(),
+                RowKey = GenerateRowKey(clientId),
 
-			return result;
-		}
+            };
+        }
+    }
 
-		public async Task GetWalletsByChunkAsync(Func<IEnumerable<KeyValuePair<string, IEnumerable<IWallet>>>, Task> chunkCallback)
-		{
+    public class WalletsRepository : IWalletsRepository
+    {
+        private readonly INoSQLTableStorage<WalletEntity> _tableStorage;
 
-			await _tableStorage.GetDataByChunksAsync(async chunk =>
-			{
-				var yeldResult = new List<KeyValuePair<string, IEnumerable<IWallet>>>();
+        public WalletsRepository(INoSQLTableStorage<WalletEntity> tableStorage)
+        {
+            _tableStorage = tableStorage;
+        }
 
-				foreach (var walletEntity in chunk)
-				{
-					var wallets = walletEntity.Get().Where(itm => itm.Balance != 0).ToArray();
-					if (wallets.Length > 0)
-						yeldResult.Add(new KeyValuePair<string, IEnumerable<IWallet>>(walletEntity.ClientId, wallets));
-				}
+        public async Task<IEnumerable<IWallet>> GetAsync(string traderId)
+        {
+            var partitionKey = WalletEntity.GeneratePartitionKey();
+            var rowKey = WalletEntity.GenerateRowKey(traderId);
+            var entity = await _tableStorage.GetDataAsync(partitionKey, rowKey);
 
-				if (yeldResult.Count > 0)
-				{
-					await chunkCallback(yeldResult);
-					yeldResult.Clear();
-				}
-
-			});
-		}
-	}
+            return entity == null
+                ? WalletEntity.EmptyList
+                : entity.Get();
+        }
+    }
 }
--- src/Lykke.Service.HFT.Core/Accounts/IWalletsRepository.cs
+++ src/Lykke.Service.HFT.Core/Accounts/IWalletsRepository.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Lykke.Service.HFT.Core.Accounts
@@ -16,24 +15,10 @@ namespace Lykke.Service.HFT.Core.Accounts
 		public string AssetId { get; set; }
 		public double Reserved { get; set; }
 		public double Balance { get; set; }
-
-		public static Wallet Create(string assetId, double balance = 0)
-		{
-			return new Wallet
-			{
-				AssetId = assetId,
-				Balance = balance
-			};
-		}
 	}
 
 	public interface IWalletsRepository
 	{
 		Task<IEnumerable<IWallet>> GetAsync(string clientId);
-		Task<IWallet> GetAsync(string clientId, string assetId);
-		Task UpdateBalanceAsync(string clientId, string assetId, double balance);
-		Task<Dictionary<string, double>> GetTotalBalancesAsync();
-
-		Task GetWalletsByChunkAsync(Func<IEnumerable<KeyValuePair<string, IEnumerable<IWallet>>>, Task> chunk);
 	}
 }
