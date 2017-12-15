27aaa1a39893b2f94ca1918c9848d56129a65c72
tarurar <tarutin_andrew@mail.ru>
2017-12-15 16:44:01 +0300

    added history contracts (stub)

---
+++ src/Lykke.Service.HFT/Controllers/HistoryController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Lykke.Service.HFT.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Lykke.Service.HFT.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class HistoryController : Controller
+    {
+        /// <summary>
+        /// Get trades
+        /// </summary>
+        /// <param name="assetId">Asset identifier</param>
+        /// <param name="take">How many maximum items have to be returned</param>
+        /// <param name="skip">How many items skip before returning</param>
+        /// <returns></returns>
+        [HttpGet("trades")]
+        [SwaggerOperation("GetTrades")]
+        [ProducesResponseType(typeof(IEnumerable<HistoryTradeModel>), (int) HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorResponse), (int) HttpStatusCode.BadRequest)]
+        public Task<IActionResult> GetTrades([FromQuery] string assetId, [FromQuery] int take, [FromQuery] int skip)
+        {
+            throw  new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get trade details by id
+        /// </summary>
+        /// <param name="tradeId">Trade identifier</param>
+        /// <returns></returns>
+        [HttpGet("trades/{tradeId}")]
+        [SwaggerOperation("GetTrade")]
+        [ProducesResponseType(typeof(HistoryTradeModel), (int) HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorResponse), (int) HttpStatusCode.BadRequest)]
+        public Task<IActionResult> GetTrade(string tradeId)
+        {
+            throw  new NotImplementedException();
+        }
+    }
+}
---
+++ src/Lykke.Service.HFT/Models/HistoryTradeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lykke.Service.HFT.Models
+{
+    public class HistoryTradeModel
+    {
+        public string Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string LimitOrderId { get; set; }
+        public string MarketOrderId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Volume { get; set; }
+        public string AssetId { get; set; }
+        public string State { get; set; }
+    }
+}
