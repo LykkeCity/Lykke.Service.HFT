290c28f3e50b956b01774d0f20003d26c0151f42
Niek Jannink <info@jannink-it.eu>
2018-04-30 07:30:19 -0500

    Benchmark tests for HFT api

---
+++ tests/Lykke.Service.HFT.Benchmarks/Benchmarks.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace Lykke.Service.HFT.Benchmarks
+{
+    public static class Benchmarks
+    {
+        public static Platform Strategy { get; private set; }
+        public static object Config { get; private set; }
+
+        public static void Main()
+        {
+            BenchmarkRunner.Run<HftProductionBenchmark>();
+
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to exit");
+            Console.ReadLine();
+        }
+    }
+}
