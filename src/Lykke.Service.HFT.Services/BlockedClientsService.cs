99954f52949a8c100e0f91f247f64dcb45a5c970
Konstantin Ryazantsev <kj.ryazancev@gmail.com>
2024-03-20 08:47:24 +0300

    LM-3196 added handling of not existing clients

--- src/Lykke.Service.HFT.Services/BlockedClientsService.cs
+++ src/Lykke.Service.HFT.Services/BlockedClientsService.cs
@@ -1,4 +1,7 @@
 ﻿using System.Threading.Tasks;
+using Common.Log;
+using Lykke.Common.ApiLibrary.Exceptions;
+using Lykke.Common.Log;
 using Lykke.Service.ClientAccount.Client;
 using Lykke.Service.HFT.Core.Services;
 
@@ -6,16 +9,30 @@ namespace Lykke.Service.HFT.Services
 {
     public class BlockedClientsService : IBlockedClientsService
     {
+        private readonly ILog _log;
         private readonly IClientAccountClient _clientAccountClient;
 
-        public BlockedClientsService(IClientAccountClient clientAccountClient)
+        public BlockedClientsService(ILogFactory logFactory, IClientAccountClient clientAccountClient)
         {
+            _log = logFactory.CreateLog(this);
             _clientAccountClient = clientAccountClient;
         }
 
         public async Task<bool> IsClientBlocked(string clientId)
         {
-            return (await _clientAccountClient.ClientSettings.GetCashOutBlockSettingsAsync(clientId))?.TradesBlocked == true;
+            try
+            {
+                return (await _clientAccountClient.ClientSettings.GetCashOutBlockSettingsAsync(clientId))?.TradesBlocked == true;
+            } 
+            catch(ClientApiException ex) when (ex.Message == "Client not found")
+            {
+                _log.Warning("Client not found. It will be treated as blocked one", context: new
+                {
+                    ClientId = clientId
+                });
+
+                return false;
+            }
         }
     }
 }
